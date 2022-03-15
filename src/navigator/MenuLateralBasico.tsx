109a89683154c22cf9ab44e8c7037cb02ff4adef
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2022-03-15 13:28:47 -0500

    navigation tutorial

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -9,15 +9,15 @@ import { useWindowDimensions } from 'react-native';
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
-  
-    const { width } = useWindowDimensions();
 
-    return (
+  const { width } = useWindowDimensions();
+
+  return (
     <Drawer.Navigator
-      drawerType={ width >= 768 ? 'permanent' : 'front' }
+      drawerType={width >= 768 ? 'permanent' : 'front'}
     >
-      <Drawer.Screen name="StackNavigator" options={{ title: 'Home' }} component={ StackNavigator } />
-      <Drawer.Screen name="SettingsScreen" options={{ title: 'Settings' }} component={ SettingsScreen } />
+      <Drawer.Screen name="Settings" options={{ title: 'Home' }} component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" options={{ title: 'Settings' }} component={SettingsScreen} />
     </Drawer.Navigator>
   );
 }
